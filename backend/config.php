7f691e3a039d1545aaae21e13100d4dfe8bab270
Taja Kompan <tajakompan545@gmail.com>
2026-03-30 15:42:22 +0200

    h

--- backend/config.php
+++ backend/config.php
@@ -4,28 +4,13 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-if (isset($_ENV['DATABASE_URL']) && !empty($_ENV['DATABASE_URL'])) {
-    $url = $_ENV['DATABASE_URL'];
-    $dbparts = parse_url($url);
+$servername = "localhost";
+$username = "familoop_familoop";
+$password = "familoop_user";
+$dbname = "familoop_family_manager";
 
-    $conn = new mysqli(
-        $dbparts['host'],
-        $dbparts['user'],
-        $dbparts['pass'],
-        ltrim($dbparts['path'], '/'),
-        $dbparts['port']
-    );
-} else {
-    // lokalno (XAMPP)
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "family_manager";
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
-}
-
-// Preveri povezavo
 if ($conn->connect_error) {
     die("Povezava ni uspela: " . $conn->connect_error);
 }
